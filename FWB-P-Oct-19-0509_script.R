0b218d0c735067e8a6c6606c34eca7bf8566e88a
GSBurg <93785429+GSBurg@users.noreply.github.com>
2021-11-12 07:43:54 +0100

    Update FWB-P-Oct-19-0509_script.R

--- FWB-P-Oct-19-0509_script.R
+++ FWB-P-Oct-19-0509_script.R
@@ -9,19 +9,38 @@
 # The covariate dataset (env) is composed by 4 environmental numerical variables (flow velocity, water depth, substrate size and benthic organic matter) and by spatial coordinates within each grid
 
 
-## Import the datasets===============================================
-env <- read.table("env.txt", head=T) #dataset of environmental variables, spatial coordinates and invertebrate metrics
-macro <- read.table("macro.txt", head=T) #community matrix
-xy <- env[, c(3,4)] #create a dataset of coordinates by subsetting the environmental dataset
-env$campaign <- as.factor(paste(env$river, env$season, sep="_"))
+## Generate the datasets===============================================
+macro <- matrix(rnbinom(n = 4500, mu = 1, size = 1), nrow = 300)
+rownames(macro) <- paste("sample", 1:300, sep = "_")
+colnames(macro) <- paste("taxon", 1:15, sep = "_")
+
+campaign <- c(rep("camp1", 50), rep("camp2", 50), rep("camp3", 50), rep("camp4", 50), rep("camp5", 50), rep("camp6", 50))
+x <- runif(300, min = 1, max = 10)
+y <- runif(300, min = 1, max = 10)
+vel <- abs(rnorm(300, mean = 0.5, sd = 0.25))
+depth <- abs(rnorm(300, mean = 25, sd = 14))
+BOM <- abs(rnorm(300, mean = 0.2, sd = 0.6))
+sub <- abs(rnorm(300, mean = 18, sd = 17))
+env <- as.data.frame(cbind(campaign, x, y, vel, depth, sub, BOM))
+rownames(env) <- rownames(macro)
+
+env$campaign <- as.factor(env$campaign)
+env$x <- as.numeric(env$x)
+env$y <- as.numeric(env$y)
+env$vel <- as.numeric(env$vel)
+env$depth <- as.numeric(env$depth)
+env$sub <- as.numeric(env$sub)
+env$BOM <- as.numeric(env$BOM)
 
 
 ## GAM models========================================================
+library(vegan)
 library(mgcv)
 
 # the following model is for taxon richness, for the other models just replace "ntaxa" in the formula with the name of the other macroinvertebrate metrics
 # the smoothing function is applied to coordinates
 # the "by" option allow to produce a different smooth for each factor level
+env$ntaxa <- specnumber(macro)
 
 metric.gam <- gam(log(ntaxa) ~ campaign + BOM + vel + depth + sub + 
                    s(x, y, by = campaign), 
@@ -35,15 +54,14 @@ anova(metric.gam)
 # the following scripts are modified from Tikhonov et al. (2020) "Joint species distribution modelling with the r-package Hmsc"
 # https://doi.org/10.1111/2041-210X.13345
 
-## load libraries
+
 set.seed(1)
-source("load_libraries.r")
-source("evaluateModelFit.r")
+library(Hmsc)
 
 ## assign the datasets
 Y <- macro
-X <- env[,5:8] #create a dataset for environmental covariates
-grid_coords <- env[,3:4]
+X <- env[,4:7] #create a dataset for environmental covariates
+grid_coords <- env[,2:3]
 
 # a different HMSC model is done for each sampling campaign
 # subsets for each sampling campaign is created from the global datasets
@@ -69,11 +87,13 @@ rL_camp <- HmscRandomLevel(sData = sRL_camp) #sData argument for spatial data
 rL_camp <- setPriors(rL_camp, nfMin = 5, nfMax = 10)
 
 ## construct and fit the model
-thin = 100
-samples = 1000
-nChains = 4
+thin = 1 #100
+samples = 10 #1000
+nChains = 2 #4
+
+
 Y_camp_red <- as.matrix(Y_camp_red) 
-Hmsc_model_camp_red <- Hmsc(Y=Y_camp_red, XFormula=~vel+depth+sub+BOM, XData=X_camp, XScale=FALSE,
+Hmsc_model_camp_red <- Hmsc(Y=Y_camp_red, XFormula=~vel+depth+sub+BOM, XData=X_camp, XScale=TRUE,
                           distr = "lognormal poisson", studyDesign = studyDesign_camp, ranLevels = list(replica=rL_camp))
 
 # be careful, time consuming
@@ -98,14 +118,14 @@ mixing_camp_red = list(es.beta=es.beta_camp_red, ge.beta=ge.beta_camp_red,
                      es.gamma=es.gamma_camp_red, ge.gamma=ge.gamma_camp_red,
                      es.V=es.V_camp_red, ge.V=ge.V_camp_red,
                      es.omega=es.omega_camp_red, ge.omega=ge.omega_camp_red)
-mixing_camp_red
+
 
 ## compute model fit
 m <- Hmsc_model_camp_red
 predY_camp_red = computePredictedValues(Hmsc_model_camp_red, expected=FALSE)
 MF_camp_red = evaluateModelFit(hM=m, predY=predY_camp_red)
-MF_camp_red$SR2$A
-MF_camp_red$O.AUC$A
+#MF_camp_red$SR2$A
+#MF_camp_red$O.AUC$A
 
 ## compute model fit with 5 folds cross-validation
 m <- Hmsc_model_camp_red
@@ -113,8 +133,8 @@ partition_camp_CV_red=createPartition(hM=m, nfolds=5, column="replica")
 # be careful, time consuming
 predY_camp_CV_red = computePredictedValues(m, expected=FALSE, partition=partition_camp_CV_red)
 MF_camp_CV_red = evaluateModelFit(hM=m, predY=predY_camp_CV_red)
-MF_camp_CV_red$SR2$A
-MF_camp_CV_red$O.AUC$A
+#MF_camp_CV_red$SR2$A
+#MF_camp_CV_red$O.AUC$A
 
 ## plot variance partitioning
 group = c(1,2,3,4,5)
@@ -136,9 +156,10 @@ barplot(VP_camp_red$vals[-1,], horiz=TRUE, las=1, col=c("navy","green3","gray50"
         )
 
 ## plot association networks
+library(corrplot)
+
 OmegaCor_camp_red = computeAssociations(Hmsc_model_camp_red)
 supportLevel = 0.90
-
 plotOrder_camp_red = corrMatOrder(OmegaCor_camp_red[[1]]$mean,order="AOE")
 toPlot_camp_red = ((OmegaCor_camp_red[[1]]$support>supportLevel) + (OmegaCor_camp_red[[1]]$support<(1-supportLevel))>0)*OmegaCor_camp_red[[1]]$mean
 corrplot(toPlot_camp_red[plotOrder_camp_red,plotOrder_camp_red], diag = F, type = "lower", tl.col="black", tl.cex=0.7, cex.main=1.3, tl.srt = 45, mar=c(0.1,0.1,1,0.1), col = colorRampPalette(c("blue", "white", "red"))(200))
